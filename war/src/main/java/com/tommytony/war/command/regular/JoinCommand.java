5b040c2d3b573b940b699169cb230d3e63b6eb45
James Wang <jameswang9909@hotmail.com>
2018-02-27 19:05:43 -0600

    Rework the join command
    /join will autoassign players to a team

--- war/src/main/java/com/tommytony/war/command/regular/JoinCommand.java
+++ war/src/main/java/com/tommytony/war/command/regular/JoinCommand.java
@@ -30,21 +30,10 @@ public class JoinCommand extends AbstractWarCommand {
         Player player = (Player) this.getSender();
 
         Warzone zone;
-        TeamKind kind;
-        boolean signup = false;
-        if (this.args.length == 2) {
-            // zone by name
-            zone = Warzone.getZoneByName(this.args[0]);
-            kind = TeamKind.teamKindFromString(this.args[1]);
-        } else if (this.args.length == 3 && args[0].equals("delayed")) {
-            signup = true;
-            zone = Warzone.getZoneByName(this.args[1]);
-            kind = TeamKind.teamKindFromString(this.args[2]);
-        } else if (this.args.length == 1) {
+        if (this.args.length == 0) {
             zone = getWarzoneByLocation();
-            kind = TeamKind.teamKindFromString(this.args[0]);
         } else {
-            return false;
+            zone = Warzone.getZoneByName(args[0]);
         }
         if (zone == null) {
             return false;
@@ -53,35 +42,10 @@ public class JoinCommand extends AbstractWarCommand {
             this.badMsg("join.disabled");
         } else if (zone.isReinitializing()) {
             this.badMsg("join.disabled");
-        } else if (zone.getWarzoneConfig().getBoolean(WarzoneConfig.AUTOASSIGN)) {
-            this.badMsg("join.aarequired");
         } else if (!zone.getWarzoneConfig().getBoolean(WarzoneConfig.JOINMIDBATTLE) && zone.isEnoughPlayers()) {
             this.badMsg("join.progress");
         } else {
-            Team team = zone.getTeamByKind(kind);
-            if (kind == null || team == null) {
-                this.badMsg("join.team404");
-            } else if (!War.war.canPlayWar(player, team)) {
-                this.badMsg("join.permission.single");
-            } else if (team.isFull()) {
-                this.badMsg("join.full.single", team.getName());
-            } else {
-                Team previousTeam = Team.getTeamByPlayerName(player.getName());
-                if (previousTeam != null) {
-                    if (previousTeam == team) {
-                        this.badMsg("join.selfteam");
-                        return true;
-                    }
-                    previousTeam.removePlayer(player);
-                    previousTeam.resetSign();
-                }
-                if (signup && !zone.testEnoughPlayers(kind, false)) {
-                    // player wants to automatically join the zone when everyone else is ready
-                    zone.signup(player, team);
-                    return true;
-                }
-                zone.assign(player, team);
-            }
+            zone.autoAssign(player);
         }
         return true;
     }
